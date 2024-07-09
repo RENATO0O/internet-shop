4636e729f88cb6b2d5aee580807f7956d853d717
RENATO0O <882391@mail.ru>
2024-07-09 23:17:31 +0500

    Что-то сделал

--- internetshop/shop/views.py
+++ internetshop/shop/views.py
@@ -1,6 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from .models import Product
+
 # Create your views here.
 def home(request):
-    return render(request, 'index.html')
+    products = Product.object.all()
+
+    return render(request, 'index.html', {
+        'products': products
+    })
