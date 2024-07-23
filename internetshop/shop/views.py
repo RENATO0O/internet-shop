ce45e43024285e454531b844665ca2bc348c20a9
RENATO0O <882391@mail.ru>
2024-07-23 22:34:00 +0500

    Добавил сохранение поиска

--- internetshop/shop/views.py
+++ internetshop/shop/views.py
@@ -13,7 +13,8 @@ def home(request):
         products = Product.objects.all()
 
     return render(request, "index.html", {
-        'products': products
+        'products': products,
+        'search': search,
     })
 
 def view_product(request, id):
